ddc658ede9141972cf038b924efd926f1b7cf57c
Umbr0o <slimshady1904@mail.ru>
2022-06-01 18:31:22 +0300

    Added new xcode project

---
+++ Diplom-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diplom-project
+//
+//  Created by Артем Томило on 1.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
